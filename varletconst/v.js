d6c122e6fe50dd6e1d797dfa20d799c383a04a46
zachpc <zachzebray@gmail.com>
2025-10-03 22:04:01 -0400

    lots of linting

---
+++ varletconst/v.js
@@ -0,0 +1,31 @@
+// var greeter = "hey hi";
+
+// function myFunction() {
+//   var hello = "hello"
+// }
+
+// console.log(greeter);
+
+// console.log(greeter);
+
+// var greeter = "hey hi";
+// var greeter = "say hello instead"
+
+// let greeter = "hey hi"
+// if (true) {
+//   let hello = "say hello instead";
+//   console.log(hello);
+// }
+
+// console.log(hello);
+
+// console.log(greeter);
+// let greeter = "hey hi";
+
+// const greeter = {
+//   message: "say hi",
+//   times: 4
+// }
+// greeter.message = "say hello instead"
+// console.log(greeter.message);
+
