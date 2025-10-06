e59cd4725ed086a7da5c0732b041e352d1b93e00
zachpc <zachzebray@gmail.com>
2025-10-06 14:55:07 -0400

    js sandbox

---
+++ javascript-arrays/ararays.js
@@ -0,0 +1,12 @@
+// some() method 
+ 
+// const myArray = ["a", "b", "c", "d", "e"]
+
+// console.log(myArray.some(test => test === "d")
+// )
+
+//reduce()
+
+const myArray = [1, 2, 3, 4, 5];
+
+console.log(myArray.reduce((total,value) => total * value))
