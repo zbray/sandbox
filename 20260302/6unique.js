f3f5d5692ded35911dec8403149e0e62d09f91da
Zach Bray <zachzebray@gmail.com>
2026-03-09 14:27:08 -0400

    Start Unique sliding window challenge

---
+++ 20260302/6unique.js
@@ -0,0 +1,19 @@
+// 6. The "Unique Sequence" (Sliding Window)
+
+//    Description: Given a string, find the length of the longest continuous substring that has no repeating characters.
+//    Forbidden: .split(), .includes(), .indexOf(), or Set.
+//    Expected Output: longestUnique("abcabcbb") -> 3 (for "abc"). longestUnique("pwwkew") -> 3 (for "wke").
+
+
+function longestUnique(str) {
+
+  //initialize a left pointer, right pointer, charMap object, and max counter to track the max size of substrings
+  let left = 0;
+  let right = 0;
+  let charMap = {};
+  let max = 0;
+
+  for (let right =)
+  
+  return max;
+}
