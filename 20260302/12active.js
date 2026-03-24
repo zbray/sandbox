d59a2ad83656eb007ac84ec614510c797c01309e
Zach Bray <zachzebray@gmail.com>
2026-03-23 20:57:37 -0400

    Complete V1 and V3 of Active User challenge

---
+++ 20260302/12active.js
@@ -0,0 +1,70 @@
+// 12. The "Active User Finder" (Objects + Filter)
+
+//     Description: Given an array of user objects [{name: "Alice", active: true}, {name: "Bob", active: false}], return an array of only the objects where active is true.
+//     V1 (Pure): Use only loops.
+//     V2 (Native): Use .filter().
+//     V3 (The Flex): Use .reduce().
+
+// V1
+
+function activeUser(userArray) {
+  // initialize a blank results array
+  let results = [];
+  for (let i = 0; i < userArray.length; i++) {
+    // check for active=true and push name to results
+    if (userArray[i].active) {
+      results.push(userArray[i].name);
+    }
+  }
+  return results;
+}
+
+console.log(
+  activeUser([
+    { name: "Alice", active: true },
+    { name: "Bob", active: false },
+  ]),
+);
+
+// V2
+
+// V3
+
+function activeUser(userArray) {
+  const result = userArray.reduce(function (acc, cur) {
+    if (cur.active) {
+      acc.push(cur.name);
+    }
+    return acc;
+  }, []);
+  return result;
+}
+console.log(
+  activeUser([
+    { name: "Alice", active: true },
+    { name: "Bob", active: false },
+  ]),
+);
+
+// I hit a snag here by using the following:
+//     if (userArray[i].active) {
+
+// [i] was not defined - I should have used `cur` for `userArray[i]
+// I learned that I should have used the callback's own parameters instead of trying index an undefined variable
+
+// I also did not initially create an else statement for when the if statement is false and Bob returned undefined
+// I learned that reduce always needs to return acc otherwise it becomes undefined, breaking the chain
+
+// my first solve for the undefined if false bug was to have an else statement:
+// if (cur.active) {
+//   acc.push(cur.name);
+//   return acc;
+// } else {
+//   return acc;
+// }
+
+// But since both the if and else statements held `return acc` I hoisted it outside the if to avoid repeating code
+// if (cur.active) {
+//   acc.push(cur.name);
+// }
+// return acc;
