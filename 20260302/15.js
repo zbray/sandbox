6d78322c8f208b36bb72b4d5433bb56499a67064
zach mac <zachzebray@gmail.com>
2026-04-14 21:50:16 -0400

    Start problem 15

--- 20260302/15.js
+++ 20260302/15.js
@@ -1,6 +1,8 @@
 // 15. The "Tree Builder" (Flat to Hierarchical)
 
-//     Description: Databases return "flat" data, but UI components (like dropdown menus or comment threads) need "nested" trees. Take a flat array of categories and build a nested object tree.
+// Description: Databases return "flat" data, but UI components 
+//   (like dropdown menus or comment threads) need "nested" trees. 
+//   Take a flat array of categories and build a nested object tree.
 
 // The Input:
 
@@ -11,5 +13,29 @@
 //   { id: 4, name: "Books", parentId: null }
 // ];
 
-//     Expected Output: An object or array where "Computers" is nested inside the children array of "Electronics", and "Laptops" is inside "Computers".
-//     Logic Concept: This tests object references and Hash Maps. If he tries to solve this with raw nested loops, it will be a nightmare. He needs to learn to create a "Lookup Map" (an object mapping IDs to the items) to assemble the tree efficiently.
+// Expected Output: An object or array where "Computers" 
+//    is nested inside the children array of "Electronics", and
+//    "Laptops" is inside "Computers".
+// Logic Concept: This tests object references and Hash Maps. 
+//    If he tries to solve this with raw nested loops, it will be a 
+//    nightmare. He needs to learn to create a "Lookup Map" (an object 
+//    mapping IDs to the items) to assemble the tree efficiently.
+  
+const flat = [
+  { id: 1, name: "Electronics", parentId: null },
+  { id: 2, name: "Computers", parentId: 1 },
+  { id: 3, name: "Laptops", parentId: 2 },
+  { id: 4, name: "Books", parentId: null }
+];
+
+// starting with 
+// function buildTree(flat) {
+
+// }
+const map = {};
+
+flat.forEach(item => {
+  map[item.id] = item;
+});
+
+console.log(map);
